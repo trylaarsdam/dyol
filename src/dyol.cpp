d363eb13f0773b4f81eae10de6eff53c1f38c224
trylaarsdam <50586094+trylaarsdam@users.noreply.github.com>
2019-07-11 16:45:52 -0500

    Add files via upload
    Webhook for Dark Sky fixed and working. Google Maps Geolocator IP.

--- src/dyol.cpp
+++ src/dyol.cpp
@@ -9,6 +9,8 @@ void externalTempFunction();
 void displayManagementFunction();
 void blynkDataPushFunction();
 void nfcPairingFunction();
+void locationCallback(float lat, float lon, float accuracy);
+void tempHandler(const char *event, const char *data);
 void setup();
 void loop();
 #line 1 "c:/Users/Nerdom/Desktop/dyol/src/dyol.ino"
@@ -16,9 +18,19 @@ SYSTEM_THREAD(ENABLED);
 
 #include <blynk.h>
 #include <ITEADLIB_Nextion.h>
+#include <google-maps-device-locator.h>
 #include <Adafruit_DHT_Particle.h>
-#include <HttpClient.h>
+//#include <HttpClient.h>
 #include <Nextion.h>
+#include <unistd.h>
+#include <cstdio>
+#include <JsonParserGeneratorRK.h>
+
+SerialLogHandler logHandler;
+GoogleMapsDeviceLocator locator;
+USARTSerial& nexSerial = Serial1;
+unsigned int nextTime = 30;    // Next time to contact the server
+//HttpClient http;
 
 #define internalTempValue A2
 #define rawEncoderValue A4
@@ -32,6 +44,14 @@ Thread displayManagementThread; //handles UI and encoder interpret
 Thread blynkDataPushThread;
 Thread nfcPairingThread;
 
+NexPage page0 = NexPage(0, 0, "page0");
+NexPage page1 = NexPage(1, 0, "page1");
+NexPage page2 = NexPage(2, 0, "page2");
+NexPage page3 = NexPage(3, 0, "page3");
+
+float global_lat = 0;
+float global_lon = 0;
+
 void internalTempFunction() {
   while(1){
     //display internal temp
@@ -42,9 +62,12 @@ void internalTempFunction() {
 
 void externalTempFunction() {
   while(1){
-    //display external temp
+    //display external temp     SPRINTF
     //double temp = https://api.weatherbit.io/v2.0/current?city=Lombard,NC&key=c175924274b5402e8413adbee368dd35;
-
+    char data[64];
+    sprintf(data, "%f,%f", global_lat, global_lon);
+    Particle.publish("tempRead", data, PRIVATE);
+    delay(10000);
   }
 }
 
@@ -53,6 +76,14 @@ void displayManagementFunction() {
     //display managment (page select)
       //interpret encoder
     //touch == "enter"
+    page0.show();
+    delay(500);
+    page1.show();
+    delay(500);
+    page2.show();
+    delay(500);
+    page3.show();
+    delay(500);
   }
 }
 
@@ -72,6 +103,20 @@ void nfcPairingFunction() {
   }
 }
 
+void locationCallback(float lat, float lon, float accuracy) {
+  // Handle the returned location data for the device. This method is passed three arguments:
+  // - Latitude
+  // - Longitude
+  // - Accuracy of estimated location (in meters)
+  global_lat = lat;
+  global_lon = lon;
+}
+
+void tempHandler(const char *event, const char *data) {
+  // Handle the integration response
+  Serial.println("Recieved.");
+}
+
 void setup() {
   nfcPairingThread = Thread("nfcPair", nfcPairingFunction);
   blynkDataPushThread = Thread("blynkPush", blynkDataPushFunction);
@@ -80,6 +125,8 @@ void setup() {
   internalTempThread = Thread("internalTemp", internalTempFunction);
   pinMode(internalTempValue, INPUT);
   Serial.begin();
+  locator.withSubscribe(locationCallback).withLocatePeriodic(4800);
+  Particle.subscribe("hook-response/tempRead", tempHandler, MY_DEVICES);
 }
 
 
@@ -88,5 +135,5 @@ void loop() {
   if(button_touched == 1){
     Serial.println("button_on");
   }
-  
+  locator.loop();
 }
--- src/dyol.ino
+++ src/dyol.ino
@@ -2,9 +2,19 @@ SYSTEM_THREAD(ENABLED);
 
 #include <blynk.h>
 #include <ITEADLIB_Nextion.h>
+#include <google-maps-device-locator.h>
 #include <Adafruit_DHT_Particle.h>
-#include <HttpClient.h>
+//#include <HttpClient.h>
 #include <Nextion.h>
+#include <unistd.h>
+#include <cstdio>
+#include <JsonParserGeneratorRK.h>
+
+SerialLogHandler logHandler;
+GoogleMapsDeviceLocator locator;
+USARTSerial& nexSerial = Serial1;
+unsigned int nextTime = 30;    // Next time to contact the server
+//HttpClient http;
 
 #define internalTempValue A2
 #define rawEncoderValue A4
@@ -18,6 +28,14 @@ Thread displayManagementThread; //handles UI and encoder interpret
 Thread blynkDataPushThread;
 Thread nfcPairingThread;
 
+NexPage page0 = NexPage(0, 0, "page0");
+NexPage page1 = NexPage(1, 0, "page1");
+NexPage page2 = NexPage(2, 0, "page2");
+NexPage page3 = NexPage(3, 0, "page3");
+
+float global_lat = 0;
+float global_lon = 0;
+
 void internalTempFunction() {
   while(1){
     //display internal temp
@@ -28,9 +46,12 @@ void internalTempFunction() {
 
 void externalTempFunction() {
   while(1){
-    //display external temp
+    //display external temp     SPRINTF
     //double temp = https://api.weatherbit.io/v2.0/current?city=Lombard,NC&key=c175924274b5402e8413adbee368dd35;
-
+    char data[64];
+    sprintf(data, "%f,%f", global_lat, global_lon);
+    Particle.publish("tempRead", data, PRIVATE);
+    delay(10000);
   }
 }
 
@@ -39,6 +60,14 @@ void displayManagementFunction() {
     //display managment (page select)
       //interpret encoder
     //touch == "enter"
+    page0.show();
+    delay(500);
+    page1.show();
+    delay(500);
+    page2.show();
+    delay(500);
+    page3.show();
+    delay(500);
   }
 }
 
@@ -58,6 +87,20 @@ void nfcPairingFunction() {
   }
 }
 
+void locationCallback(float lat, float lon, float accuracy) {
+  // Handle the returned location data for the device. This method is passed three arguments:
+  // - Latitude
+  // - Longitude
+  // - Accuracy of estimated location (in meters)
+  global_lat = lat;
+  global_lon = lon;
+}
+
+void tempHandler(const char *event, const char *data) {
+  // Handle the integration response
+  Serial.println("Recieved.");
+}
+
 void setup() {
   nfcPairingThread = Thread("nfcPair", nfcPairingFunction);
   blynkDataPushThread = Thread("blynkPush", blynkDataPushFunction);
@@ -66,6 +109,8 @@ void setup() {
   internalTempThread = Thread("internalTemp", internalTempFunction);
   pinMode(internalTempValue, INPUT);
   Serial.begin();
+  locator.withSubscribe(locationCallback).withLocatePeriodic(4800);
+  Particle.subscribe("hook-response/tempRead", tempHandler, MY_DEVICES);
 }
 
 
@@ -74,5 +119,5 @@ void loop() {
   if(button_touched == 1){
     Serial.println("button_on");
   }
-  
+  locator.loop();
 }
